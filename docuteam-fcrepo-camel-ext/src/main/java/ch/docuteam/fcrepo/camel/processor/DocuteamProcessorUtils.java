f52e9935af4399a20929d613e418dbb0d7db06e3
Vincent Decorges <v.decorges@docuteam.ch>
2024-12-05 13:42:02 +0100

    Give the correct credit!

--- docuteam-fcrepo-camel-ext/src/main/java/ch/docuteam/fcrepo/camel/processor/DocuteamProcessorUtils.java
+++ docuteam-fcrepo-camel-ext/src/main/java/ch/docuteam/fcrepo/camel/processor/DocuteamProcessorUtils.java
@@ -14,7 +14,7 @@ import org.slf4j.Logger;
  * This class provide a deleteWhere util method that is tied to the
  * docuteam datamodel compare to the standard one provided in ProcessorUtils
  *
- * @author Vincent Decorges
+ * @author Basil Marti
  */
 public class DocuteamProcessorUtils {
 
